8f5ddc5e7839834a2ad5a2e53b2f649db15eb19e
BillGao <billgao@sinbon.com>
2020-12-21 09:06:34 +0800

    update jpeg.py

--- Week_2/jpeg.py
+++ Week_2/jpeg.py
@@ -2,7 +2,7 @@
 Function Name: jpeg.py
 *
 Description: In this programme, we are going to create out own jpeg compression.
-             For better understanding, we will do it with OOP, which may not be the
+             For simplicity and better understanding, we will do it with OOP, which may not be the
              most efficient way to implement this method.
 *
 Edited by: [date] [author name]
@@ -12,14 +12,26 @@ import numpy as np
 
 class jpeg():
     def __init__(self, image_in):
+        assert len(image_in.shape) > 2, "shape of image_in should be greater than 2"
+
+        # setting constant
         self.__image_in = image_in
-        assert len(self.__image_in.shape) > 2, "shape of image_in should be greater than 2"
-        self.__rows = self.__image_in.shape[0]
-        self.__cols = self.__image_in.shape[1]
-        self.__channels = self.__image_in.shape[2]
+        self.__ROWS = self.__image_in.shape[0]
+        self.__COLS = self.__image_in.shape[1]
+        self.__CHANNELS = self.__image_in.shape[2]
+        self.__JPEG_TABLE = np.array([[16,11,10,16,24,40,51,61],
+                                      [12,12,14,19,26,58,60,55],
+                                      [14,13,16,24,40,57,69,56],
+                                      [14,17,22,29,51,87,80,62],
+                                      [18,22,37,56,68,109,103,77],
+                                      [24,35,55,64,81,104,113,92],
+                                      [49,64,78,87,103,121,120,101],
+                                      [72,92,95,98,112,100,103,99]],
+                                      dtype=np.float32)
 
         self.__divide()
         self.__dct()
+        self.__quantize()
 
     def __divide(self):
         """
@@ -34,8 +46,8 @@ class jpeg():
         Edited by: [date] [author name]
         """        
         copy_image = self.__image_in.copy()
-        row_block = int(self.__rows/8)
-        col_block = int(self.__cols/8)
+        row_block = int(self.__ROWS/8)
+        col_block = int(self.__COLS/8)
         self.__blocks = []
         # print(f"row_block: {row_block}, col_block: {col_block}")
         for row in range(row_block):
@@ -46,7 +58,14 @@ class jpeg():
     def __dct(self):
         self.__dct = []
         for block in self.__blocks:
-            self.__dct.append(cv2.dct(block))
+            block = np.float32(block)
+            self.__dct.append([cv2.dct(block[:,:,0]), cv2.dct(block[:,:,1]), cv2.dct(block[:,:,2])])
+    
+    def __quantize(self):
+        self.__quantization = []
+        result = cv2.divide(self.__dct[0][0], self.__JPEG_TABLE)        
+        
+
 
         
 
